6ba8e6efcff1b5b9470d1fc67518824d930f90e4
oriver <63389236+o-river@users.noreply.github.com>
2023-12-03 01:06:39 +0900

    detalabelの導入

--- config.js
+++ config.js
@@ -3,6 +3,9 @@ config = {
 	data: {
 		datasets: []
 	},
+	plugins: [
+		ChartDataLabels
+	],
 	options: {
 		responsive: true,
 		maintainAspectRatio: false,
@@ -31,6 +34,28 @@ config = {
 					pointStyle: 'line'
 					
 				}
+			},
+			datalabels: {
+				backgroundColor: context => context.dataset.borderColor,
+        padding: 4,
+        borderRadius: 4,
+        clip: true,
+        color: 'white',
+        font: {
+          weight: 'bold'
+        },
+				formatter: function(value, context) {
+					// console.log(context);
+					if(datalabelSwitch) return Math.round(value.y);
+					if(context.dataIndex < 2) return '-';
+					else {
+						var t0 = context.dataset.data[context.dataIndex    ].x;
+						var t1 = context.dataset.data[context.dataIndex - 1].x;
+						var t2 = context.dataset.data[context.dataIndex - 2].x;
+						return (Math.round((t0 - t1)/(t1 - t2)*100)/100).toFixed(2);
+					}
+				}
+        // formatter: (value, context) => datalabelSwitch ? Math.round(value.y) : context.detaset.data[-1].x
 			}
 		},
 		scales: {
@@ -38,14 +63,14 @@ config = {
 				type: 'realtime',
 				realtime: {
 					duration: 20000,
-					// onRefresh: chart => {
-					// 	chart.data.datasets.forEach(dataset => {
-					// 		dataset.data.push({
-					// 			x: Date.now(),
-					// 			y: Math.random() * 1024
-					// 		});
-					// 	});
-					// }
+					onRefresh: chart => {
+						chart.data.datasets.forEach(dataset => {
+							dataset.data.push({
+								x: Date.now(),
+								y: Math.random() * 1024
+							});
+						});
+					}
 				},
 				titie: {
 					display: true,
